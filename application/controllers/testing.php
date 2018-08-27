f49f8da91000e5c519ae810249881959afcc2208
FarhanM <farhan.mry@gmail.com>
2018-08-27 08:39:20 +0700

    Setiap gambar di post di upload di folder teresendiri di ./assets/images/posts/

---
+++ application/controllers/testing.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class testing extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+    }
+
+    public function index(){
+        $this->form_validation->set_rules('nama','Nama','required');
+
+        if($this->form_validation->run() === FALSE){
+            $this->load->view('testing');
+        }else{
+            $dirname = $this->input->post('nama');
+            echo $dirname;
+            $dirname = './assets/testing/'.$dirname; 
+            echo $dirname;
+            if (!is_dir($dirname)){
+                #mkdir('.assets/testing/'.$dirname);
+                mkdir($dirname,0777,true);
+            }else{
+                echo 'exist';
+            }
+        }
+    }
+}
+
---
+++ application/views/testing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Testing Page</title>
+</head>
+<body>
+    <p>Klik tombol untuk buat folder baru</p>
+    <?php echo form_open('testing/index');?>
+       <input type="input" name="nama"><br> <br>
+
+
+       <input type="submit" name="submit" value="buat">    
+    </form>
+    
+    
+</body>
+</html>
